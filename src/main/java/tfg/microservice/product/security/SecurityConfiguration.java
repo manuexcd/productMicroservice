0a173c7d850019fcc1aa410b19a8d85cfadc5c42
Manuel  Lara <mlara@atsistemas.com>
2020-01-30 17:52:35 +0100

    Security configuration with Spring Security and JWT

---
+++ src/main/java/tfg/microservice/product/security/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package tfg.microservice.product.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.cors();
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/products/**").authenticated();
+		// http.addFilter(new JWTAuthenticationFilter(authenticationManager()));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
