c15de45b7ea18e2cfe01172dc4682baf86ebff39
Manuel  Lara <mlara@atsistemas.com>
2020-03-03 19:32:48 +0100

    mongodb url propertie

--- src/main/java/tfg/microservice/product/config/MongoConfig.java
+++ src/main/java/tfg/microservice/product/config/MongoConfig.java
@@ -1,5 +1,6 @@
 package tfg.microservice.product.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
@@ -9,6 +10,9 @@ import com.mongodb.client.MongoClients;
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+	@Value("${mongodb.url}")
+	private String mongoUrl;
+
 	@Override
 	protected String getDatabaseName() {
 		return "users";
@@ -16,7 +20,7 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
 	@Override
 	public MongoClient mongoClient() {
-		return MongoClients.create("mongodb://localhost:27017");
+		return MongoClients.create(mongoUrl);
 	}
 
 }
