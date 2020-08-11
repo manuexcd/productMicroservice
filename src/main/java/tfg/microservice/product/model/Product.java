f815e7266765dd8dde65d719e4585cc7a5787eec
Manuel  Lara <mlara@atsistemas.com>
2020-08-11 18:39:02 +0200

    gitignore

--- src/main/java/tfg/microservice/product/model/Product.java
+++ src/main/java/tfg/microservice/product/model/Product.java
@@ -12,7 +12,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document(collection = "users")
+@Document(collection = "products")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @NoArgsConstructor
